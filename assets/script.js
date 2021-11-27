4614bb31e5eeecb1742607c7f9300b97af627bf5
kelsey-schenk <kelsey.schenk@att.net>
2021-11-27 16:09:27 -0600

    added api key

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,21 @@
+var cityInput = document.querySelector("#city-name");
+var APIkey = "6cc3bdc3df946f3e2c80090de1f599ba";
+
 var getWeather = function () {
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
-    fetch(weatherUrl).then(function(response){
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid" + APIkey;
+    fetch(weatherUrl)
+        .then(function(response){
         if (response.ok) {
             console.log("response ok");
         }
     })
+};
+
+if (cityInput) {
+    getWeather();
+} else {
+    alert('Please enter a valid city');
 }
+
+getWeather();
+
